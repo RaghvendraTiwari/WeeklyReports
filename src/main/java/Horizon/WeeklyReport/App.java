d6412c8e0f0f39a993e9f77b323c451dd8df2f26
RaghvendraTiwari <raghav.tiwari@gmail.com>
2019-10-18 20:42:03 +0530

    Update App.java

--- src/main/java/Horizon/WeeklyReport/App.java
+++ src/main/java/Horizon/WeeklyReport/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Raghvendra Tiwari Ji Bhai sahab!" );
+        System.out.println( "Hello Raghvendra Tiwari !" );
     }
 }
