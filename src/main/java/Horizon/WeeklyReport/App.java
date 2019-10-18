e28e8b5edb535436f5629fcc7e27b01fc31c32b1
Raghvendra Tiwari <raghav.tiwari@gmail.com>
2019-10-18 23:03:17 +0530

    Commit for Jenkins auto build

--- src/main/java/Horizon/WeeklyReport/App.java
+++ src/main/java/Horizon/WeeklyReport/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Raghvendra Tiwari ! Namaskar JI" );
+        System.out.println( "Hello Raghvendra Tiwari ! Namaskar JI Ram Ram" );
     }
 }
